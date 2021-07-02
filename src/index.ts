f93a62e6078dee2e507ed2161e94784b4d83049b
KoinoEngineering <email@koino.engineering>
2021-07-03 08:08:03 +0900

    work
    環境変数を外だし

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+import mysql from "mysql2";
 import fs from "fs";
 import { Client } from "ssh2";
 import dotEnv from "dotenv";
@@ -7,25 +8,51 @@ dotEnv.config();
 if (process.env.DB_SSH_KEY) {
     const conn = new Client();
     conn.on("ready", () => {
-        console.log("Client :: ready");
-        conn.shell((err, stream) => {
-            if (err) throw err;
-            stream
-                .on("close", () => {
-                    console.log("Stream :: close");
-                    conn.end();
-                })
-                .on("data", (data: unknown) => {
-                    console.log("OUTPUT: " + data);
+        console.log("connection ready");
+        if (process.env.DB_HOST) {
+            conn.forwardOut("127.0.0.1", Number(process.env.DB_PORT), process.env.DB_HOST, Number(process.env.DB_PORT), (err, stream) => {
+                const mysqlConnection = mysql.createConnection({
+                    host: process.env.DB_HOST,
+                    port: Number(process.env.DB_PORT),
+                    database: process.env.DB_DATABASE,
+                    user: process.env.DB_USERNAME,
+                    password: process.env.DB_PASSWORD,
+                    stream,
                 });
-            stream.end("ls -la\nexit\n");
+                mysqlConnection.connect((err) => {
+                    if (err) {
+                        console.log("connecton filed");
+                        console.log(err);
+                    } else {
+                        console.log("connecton success");
+                    }
+                });
+                mysqlConnection.query("select * from segments", (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(result);
+                        mysqlConnection.end();
+                        conn.end();
+                    }
+                });
+            });
+        } else {
+            console.error("no DB_HOST");
+        }
+    })
+        .on("end", () => {
+            console.error("connection end");
+        })
+        .on("error", (...args) => {
+            console.error(...args);
+        })
+        .connect({
+            host: process.env.DB_SSH_HOST,
+            port: 22,
+            username: process.env.DB_SSH_USER,
+            privateKey: fs.readFileSync(process.env.DB_SSH_KEY),
         });
-    }).connect({
-        host: process.env.DB_SSH_HOST,
-        port: 22,
-        username: process.env.DB_SSH_USER,
-        privateKey: fs.readFileSync(process.env.DB_SSH_KEY),
-    });
 } else {
     console.log("no DB_SSH_KEY");
 }
